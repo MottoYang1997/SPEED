9412cc79e88256e2a051ab5f04a4cf610c6ba120
Yiming Yang <yiming.yang@kaust.edu.sa>
2024-04-15 13:48:12 +0300

    First Boost Experiment

--- Playgrounds/exp00_boost_convert_from_spice_equations.py
+++ Playgrounds/exp00_boost_convert_from_spice_equations.py
@@ -15,8 +15,8 @@ R1 3 0 100\n\
 
 
 ## Component Parameters
-R_S1 = 100e-3
-R_S2 = 10e9
+R_S1 = 100e6
+R_S2 = 10e-3
 L1 = 10e-6
 C1 = 10e-6
 R1 = 100
@@ -62,28 +62,37 @@ S_u = np.zeros((6+6+3, 1))
 S_u[0,0] = 1 # S[0] = 1 * 5
 
 ### Circuit Equation Matrix with Number of Unknowns: 6 Branch Currents, 6 Branch Voltages, 3 Node Voltages
-L0 = np.hstack((K_I, K_V, np.zeros((6,3)))) # 6x6, 6x6, 6x3
-L1 = np.hstack((np.zeros((6,6)), np.eye(6), -np.transpose(A))) # 6x6, 6x6, 6x3
-L2 = np.hstack((A, np.zeros((3,6)), np.zeros((3,3)))) # 3x6, 3x6, 3x3
-L = np.vstack((L0, L1, L2))
+EQL0 = np.hstack((K_I, K_V, np.zeros((6,3)))) # 6x6, 6x6, 6x3
+EQL1 = np.hstack((np.zeros((6,6)), np.eye(6), -np.transpose(A))) # 6x6, 6x6, 6x3
+EQL2 = np.hstack((A, np.zeros((3,6)), np.zeros((3,3)))) # 3x6, 3x6, 3x3
+EQL = np.vstack((EQL0, EQL1, EQL2))
 
 
 ## Build Up System Equations
 ### Dynamic Equations
-L_inv = np.linalg.inv(L)
-A_sys = np.linalg.inv(K_x_vie @ L_inv @ S_xdot)
-B_sys = A_sys @ K_x_vie @ L_inv @ S_u
+EQL_inv = np.linalg.inv(EQL)
+A_sys = np.linalg.inv(K_x_vie @ EQL_inv @ S_xdot)
+B_sys = A_sys @ K_x_vie @ EQL_inv @ S_u
 
 ### Observation Matrix Assembly
 K_y_vie = np.zeros((1, 6+6+3)) # y = K_y_vie
 K_y_vie[0, (6-1) + 6] = 1 # y[0] = v_R1
-K_y_x_dot = K_y_vie @ L_inv
-C_sys = K_y_x_dot @ S_xdot @ A_sys
-D_sys = K_y_x_dot @ (S_xdot @ B_sys + S_u)
+K_y_xdot = K_y_vie @ EQL_inv
+C_sys = K_y_xdot @ S_xdot @ A_sys
+D_sys = K_y_xdot @ (S_xdot @ B_sys + S_u)
 
 
-## TODO: Build Up System Equation From Hand Calculation
+## Build Up System Equation From Hand Calculation
+## Compared with standard i and v polarities of components, in this example, these polarities are reversed.
+A_calc = np.array(((R_S1*R_S2/L1/(R_S1+R_S2), R_S1/L1/(R_S1+R_S2)), (-R_S1/C1/(R_S1+R_S2), (R1+R_S1+R_S2)/R1/C1/(R_S1+R_S2))))
+B_calc = np.array(([-1/L1], [0]))
+C_calc = np.array(((0, 1)))
+D_calc = np.array(((0)))
 
+## Compare the Two Derivations
+print(A_sys - A_calc)
+print(B_sys - B_calc)
+print(C_sys - C_calc)
+print(D_sys - D_calc)
 
-## TODO: Compare the Two Derivations
-
+## TODO: automatically parse the SPICE netlist and build-up necessary circuit equations of each switch state.
